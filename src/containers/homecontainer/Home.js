0c60c067d442d3a3f7ec104347caf61a240e9693
Roberto Margain <margain.roberto@gmail.com>
2021-01-05 18:28:06 -0600

    not ready to pull request

--- src/containers/homecontainer/Home.js
+++ src/containers/homecontainer/Home.js
@@ -1,11 +1,12 @@
-import React from "react";
+import React, {Component} from "react";
+import Navbar2 from "../../components/Navbar2";
 
 export default class Home extends Component{
     render(){
         return(
             <section>
                 <div>
-                    
+                    <Navbar2/>
                 </div>
             </section>
         )
--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 //valida si la aplicacion esta en produccion
-//const isProduction = process.env.NODE_ENV === "production";
+const isProduction = process.env.NODE_ENV === "production";
 
 // si la app esta ya en produccion colocara otra ruta de lo contrario usara localhost
-//const URL =  isProduction ? "https://robert-caro-back.herokuapp.com/api" : "http://localhost:3000/api";
+const URL =  isProduction ? "https://robert-caro-back.herokuapp.com/api" : "http://localhost:3000/api";
 
 
-const URL = "https://robert-caro-back.herokuapp.com/api"
+//const URL = "https://robert-caro-back.herokuapp.com/api"
 axios.defaults.withCredentials = true;
 
 export const _axios = axios.create({

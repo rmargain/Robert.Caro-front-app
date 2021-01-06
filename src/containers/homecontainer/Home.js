1f2cda91b60e7469bcb03957255dcb62d458a58b
Lina <car.badillom@gmail.com>
2021-01-05 21:39:58 -0600

    Eliminación de navbar en containers

--- src/containers/homecontainer/Home.js
+++ src/containers/homecontainer/Home.js
@@ -1,13 +1,11 @@
 import React, {Component} from "react";
-import Navbar from "../../components/Navbar";
+
 
 export default class Home extends Component{
     render(){
         return(
             <section>
             <div>
-            HOLA
-            <Navbar/>
                 </div>
             </section>
         )

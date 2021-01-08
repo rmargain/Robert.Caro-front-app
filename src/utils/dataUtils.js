fb002f71e1fa1e2e18f00a0b644a6fc0bdc710ba
Lina <car.badillom@gmail.com>
2021-01-07 21:35:59 -0600

    utils

---
+++ src/utils/dataUtils.js
@@ -0,0 +1,16 @@
+export const normalizeData = (arr) => {
+    return arr.reduce((acc,item)=>{
+        return {...acc,[item._id]:item}
+    },{})
+}
+
+export const denormalizeData = (obj)=>{
+    return Object.values(obj)
+}
+
+//Filtro para eliminar productos
+
+export const filterItem = (items, id) =>{
+    delete items[id]
+    return items
+}

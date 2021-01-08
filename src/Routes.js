617adf94582eafaf1a767fba907158fb9a01dd54
Lina <car.badillom@gmail.com>
2021-01-07 21:30:28 -0600

    routes{product container & user profike}

--- src/Routes.js
+++ src/Routes.js
@@ -2,17 +2,22 @@ import React from "react";
 import {Switch, Route} from "react-router-dom";
 import AuthContainer from "./containers/authcontainer/Auth";
 import SignupContainer from "./containers/authcontainer/SignUp";
-import HomeContainer from "./containers/homecontainer/Home"
+import HomeContainer from "./containers/homecontainer/Home";
+import ProductContainer from "./containers/productcontainer";
+import UserProfile from "./containers/userprofile";
 
 
 
 //Mis vistas --> Containers
 const Routes = () => (
     <Switch>
-        {/* https://www.ironshop.com/  */}
+        {/* https://www.carober.com/  */}
         <Route exact path="/" component ={HomeContainer}/>
         <Route exact path="/login" component={AuthContainer} />
         <Route exact path="/signup"  component={SignupContainer}/>
+        <Route exact path="/product/new"  component={ProductContainer}   />
+        <Route exact path="/product/:id"  component={ProductContainer}   />
+        <Route exact path="/profile"  component={UserProfile}   />
     </Switch>
 );
 

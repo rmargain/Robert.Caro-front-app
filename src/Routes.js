385bc7d8e3fcc8ce3171c4a54f71beafeb5b892e
Roberto Margain <margain.roberto@gmail.com>
2021-01-14 22:38:49 -0600

    ultimo rob

--- src/Routes.js
+++ src/Routes.js
@@ -8,7 +8,6 @@ import UserProfile from "./containers/userprofile/Index";
 import StoreContainer from "./containers/storecontainer/Index"
 import StoreProfile from "./containers/storecontainer/Profile";
 import AllProducts from "./containers/productcontainer/AllProducts"
-import CartSum from "./containers/cartcontainer/index"
 
 
 //Mis vistas --> Containers
